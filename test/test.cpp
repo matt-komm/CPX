6329de84d389e4ca0cd759de25e86bbdbf826bdf
matt-komm <mkomm@cern.ch>
2016-08-21 18:42:00 +0200

    add dlopen help in test

--- test/test.cpp
+++ test/test.cpp
@@ -8,8 +8,27 @@
 #include <string>
 
 /*
+dlopen:
 If filename contains a slash ("/"), then it is interpreted as a (relative or absolute) pathname. 
 Otherwise, the dynamic linker searches for the library as follows (see ld.so(8) for further details)
+  o   (ELF only) If the executable file for the calling program
+       contains a DT_RPATH tag, and does not contain a DT_RUNPATH tag,
+       then the directories listed in the DT_RPATH tag are searched.
+
+   o   If, at the time that the program was started, the environment
+       variable LD_LIBRARY_PATH was defined to contain a colon-separated
+       list of directories, then these are searched.  (As a security
+       measure this variable is ignored for set-user-ID and set-group-ID
+       programs.)
+
+   o   (ELF only) If the executable file for the calling program
+       contains a DT_RUNPATH tag, then the directories listed in that
+       tag are searched.
+
+   o   The cache file /etc/ld.so.cache (maintained by ldconfig(8)) is
+       checked to see whether it contains an entry for filename.
+
+   o   The directories /lib and /usr/lib are searched (in that order).
 */
 
 void test_loading_default()
